60575307c9b9c48c788b150b0dc71ce85c67ff47
FranciscoYuster <yusterfrancisco@gmail.com>
2025-02-21 21:55:07 -0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Audio player with React
-
-See on live here ⬇
-
-[DEMO LIVE](https://franciscoyuster.github.io/MediaPlayer/)
--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App"; // Asegúrate de que esta importación coincide con la exportación default de App.jsx
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById("root"));
